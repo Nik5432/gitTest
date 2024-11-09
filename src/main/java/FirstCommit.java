70fc44f0ceed6cdf6d9cbcaa19e6b8ed4f7ea62a
KrozhDev <roko.mdn@gmail.com>
2024-11-09 11:48:56 +0300

    my counts 2

--- src/main/java/FirstCommit.java
+++ src/main/java/FirstCommit.java
@@ -1,5 +1,31 @@
+import com.sun.tools.jdeprscan.scan.Scan;
+
+import java.util.Scanner;
+
 public class FirstCommit {
     public static void firstCommitMethod(){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Hello " + name);
+
+        System.out.println("Enter your age: ");
+        int age = sc.nextInt();
+
+        System.out.println("Age: " + age);
+
+        sc.close();
+
+        secondCommitMethod();
+
         System.out.println("First commit");
     }
+
+    private static void secondCommitMethod(){
+        System.out.println("Second commit");
+    }
+
 }
--- src/main/java/SecondCommit.java
+++ src/main/java/SecondCommit.java
@@ -1,5 +1,20 @@
 public class SecondCommit {
     public static void secondCommitMethod(){
+
+        System.out.println("Калькулятор 2.0");
+        int a = 34;
+        int b = 56;
+
+        System.out.println(a + b);
+
+        System.out.println(a * b);
+
+        System.out.println(a / b);
+
+        System.out.println(a - b);
+
+        System.out.println(a % b);
+
         System.out.println("Second commit");
     }
 }
