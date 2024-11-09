f5d603c8547516163743bc39e34a7c12dce98bbb
nik54 <nik5432@lenta.ru>
2024-11-09 11:51:47 +0300

    commit Main.java class  3

--- src/main/java/FirstCommit.java
+++ src/main/java/FirstCommit.java
@@ -1,5 +1,5 @@
 public class FirstCommit {
     public static void firstCommitMethod(){
-        System.out.println("First commit");
+        System.out.println("First commit with changes");
     }
 }
--- src/main/java/SecondCommit.java
+++ src/main/java/SecondCommit.java
@@ -1,5 +1,9 @@
+import javax.swing.*;
+
 public class SecondCommit {
     public static void secondCommitMethod(){
-        System.out.println("Second commit");
+        System.out.println("Second commit with changes");
     }
+    JPanel jPanel = new JPanel();
+
 }
