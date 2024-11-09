b829c882012468abdda380e750f871ea41371989
KrozhDev <roko.mdn@gmail.com>
2024-11-09 11:34:24 +0300

    my counts

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,22 @@
 public class Main {
     public static void main(String[] args) {
 
+        int a = 19;
+        int b = 245;
+
+        if (a > b) {
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+
+        System.out.println(a + b);
+
+        System.out.println(a * b);
+
+        System.out.println(a / b);
+
+        System.out.println(a - b);
+
     }
 }
